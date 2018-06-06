926d236a605f1cc58002c6b1f7bc73bc7edddf74
Vit Brunner <vit.brunner@gmail.com>
2018-06-06 19:24:51 +0200

    Move out

--- src/main/scala/_05/GenomicRangeQuery.scala
+++ src/main/scala/_05/GenomicRangeQuery.scala
@@ -3,38 +3,38 @@ package _05
 import scala.annotation.tailrec
 
 object GenomicRangeQuery {
-  def solution(s: String, p: Array[Int], q: Array[Int]): Array[Int] = {
-    @tailrec
-    def getPrefixSumArray(
-      c: Char,
-      sequence: List[Char],
-      count: Int = 0,
-      lst: List[Int] = Nil
-    ): Array[Int] =
-      sequence match {
-        case h :: t if h == c =>
-          getPrefixSumArray(c, t, count + 1, count + 1 :: lst)
-        case h :: t =>
-          getPrefixSumArray(c, t, count, count :: lst)
-        case Nil =>
-          lst.reverse.toArray
-      }
+  @tailrec
+  def getPrefixSumArray(
+    c: Char,
+    sequence: List[Char],
+    count: Int = 0,
+    lst: List[Int] = Nil
+  ): Array[Int] =
+    sequence match {
+      case h :: t if h == c =>
+        getPrefixSumArray(c, t, count + 1, count + 1 :: lst)
+      case h :: t =>
+        getPrefixSumArray(c, t, count, count :: lst)
+      case Nil =>
+        lst.reverse.toArray
+    }
 
+  def isInRange(psArray: Array[Int], fromIndex: Int, toIndex: Int): Boolean = {
+    val from =
+      if (fromIndex == 0) 0
+      else psArray(fromIndex - 1)
+
+    val to = psArray(toIndex)
+
+    from - to != 0
+  }
+
+  def solution(s: String, p: Array[Int], q: Array[Int]): Array[Int] = {
     val seq = s.toList
     val a = getPrefixSumArray('A', seq)
     val c = getPrefixSumArray('C', seq)
     val g = getPrefixSumArray('G', seq)
     val t = getPrefixSumArray('T', seq)
-
-    def isInRange(psArray: Array[Int], fromIndex: Int, toIndex: Int): Boolean = {
-      val from =
-        if (fromIndex == 0) 0
-        else psArray(fromIndex - 1)
-
-      val to = psArray(toIndex)
-
-      from - to != 0
-    }
 
     def findMinInRange(index: Int): Int =
       if      (isInRange(a, p(index), q(index))) 1
