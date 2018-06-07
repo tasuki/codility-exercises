93b2b19f265c91f7f36a11143e2d47387e7d4193
Vit Brunner <vit.brunner@gmail.com>
2018-06-07 12:43:48 +0200

    Count sublists

--- src/main/scala/toptal/Toptal3.scala
+++ src/main/scala/toptal/Toptal3.scala
@@ -1,11 +1,67 @@
 package toptal
 
+import scala.annotation.tailrec
+import scala.collection.immutable.BitSet
+
 // get a list of integers: how many sublists can it be split into,
 // so that when you sort each, the whole list is sorted
 
 object Toptal3 {
-  def solution(list: Seq[Int]): Int = {
-    1
+  @tailrec
+  def findLoop(
+    arr: Vector[Int],
+    startFrom: Int,
+    ret: List[Int] = Nil
+  ): List[Int] = {
+    val newEl = arr(startFrom)
+
+    if (ret.lastOption.contains(newEl)) ret.sorted
+    else findLoop(arr, newEl, newEl :: ret)
+  }
+
+  @tailrec
+  def findLoops(
+    arr: Vector[Int],
+    included: Vector[Boolean],
+    loops: List[List[Int]] = Nil
+  ): List[List[Int]] = {
+    val startFrom = included.zipWithIndex
+      .dropWhile(_._1).headOption.map(_._2)
+
+    startFrom match {
+      case None => loops.reverse
+      case Some(start) =>
+        val loop = findLoop(arr, start)
+        val newIncl = included.zipWithIndex.map {
+          case (incl, index) =>
+            if (incl) true
+            else loop.contains(index)
+        }
+
+        findLoops(arr, newIncl, loop :: loops)
+    }
+  }
+
+  def findLoops(arr: Vector[Int]): List[List[Int]] =
+    findLoops(arr, Vector.fill(arr.length)(false))
+
+  def joinLoops(a: List[Int], b: List[Int]): List[Int] =
+    (a ++ b).sorted // TODO inefficient
+
+  def mergeConnectedLoops(loops: List[List[Int]]): List[List[Int]] = {
+    loops match {
+      case h1 :: h2 :: t =>
+        if (h1.last > h2.head)
+          mergeConnectedLoops(joinLoops(h1, h2) :: t)
+        else
+          h1 :: mergeConnectedLoops(h2 :: t)
+      case single => single
+    }
+  }
+
+  def solution(arr: Vector[Int]): Int = {
+    val loops = findLoops(arr.map(_ - 1)).map(_.map(_ + 1))
+    val connected = mergeConnectedLoops(loops)
+    connected.length
   }
 }
-
--- src/test/scala/toptal/Toptal3Test.scala
+++ src/test/scala/toptal/Toptal3Test.scala
@@ -3,7 +3,58 @@ package toptal
 import org.scalatest.FlatSpec
 
 class Toptal3Test extends FlatSpec {
-  it should "split into 2 for 2 1 4 3" in {
-    assert(Toptal3.solution(Seq(2, 1, 4, 3)) == 2)
+  it should "find 0 loop" in {
+    assert(Toptal3.findLoop(Vector(0, 1, 3, 2), 0) == Seq(0))
+  }
+  it should "find 01 loop" in {
+    assert(Toptal3.findLoop(Vector(1, 0, 3, 2), 0) == Seq(0, 1))
+  }
+  it should "find 23 loop" in {
+    assert(Toptal3.findLoop(Vector(1, 0, 3, 2), 2) == Seq(2, 3))
+  }
+  it should "find 0123 loop" in {
+    assert(Toptal3.findLoop(Vector(1, 3, 0, 2), 2) == Seq(0, 1, 2, 3))
+  }
+
+  it should "find loops" in {
+    assert(Toptal3.findLoops(Vector(0, 1, 3, 2)) == Seq(Seq(0), Seq(1), Seq(2, 3)))
+  }
+  it should "find more loops" in {
+    assert(Toptal3.findLoops(Vector(2, 1, 0, 3)) == Seq(Seq(0, 2), Seq(1), Seq(3)))
+  }
+  it should "find sorted loops" in {
+    assert(Toptal3.findLoops(Vector(2, 3, 0, 1)) == Seq(Seq(0, 2), Seq(1, 3)))
+  }
+
+  it should "split into 2 for 2143" in {
+    assert(Toptal3.solution(Vector(2, 1, 4, 3)) == 2)
+  }
+
+  it should "split into 1 for 2413" in {
+    assert(Toptal3.solution(Vector(2, 4, 1, 3)) == 1)
+  }
+
+  it should "split into 3" in {
+    assert(Toptal3.solution(Vector(1, 2, 4, 3)) == 3)
+  }
+
+  it should "split into 1" in {
+    assert(Toptal3.solution(Vector(4, 1, 2, 3)) == 1)
+  }
+
+  it should "split into 2 for 3214" in {
+    assert(Toptal3.solution(Vector(3, 2, 1, 4)) == 2)
+  }
+
+  it should "split into 1 when three circles: 13, 24, 56" in {
+    assert(Toptal3.solution(Vector(3, 5, 1, 6, 2, 4)) == 1)
+  }
+
+  it should "split into 4" in {
+    assert(Toptal3.solution(Vector(1, 3, 2, 4, 6, 5)) == 4)
+  }
+
+  it should "split into 1 when 6 and lower" in {
+    assert(Toptal3.solution(Vector(6, 1, 2, 3, 4, 5)) == 1)
   }
 }
