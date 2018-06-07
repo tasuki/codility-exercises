a8421f88476ffeed07191a787b95e22208c1c01c
Vit Brunner <vit.brunner@gmail.com>
2018-06-07 09:19:09 +0200

    Tailrec

--- src/main/scala/_05/MinAvgTwoSlice.scala
+++ src/main/scala/_05/MinAvgTwoSlice.scala
@@ -1,22 +1,32 @@
 package _05
 
+import scala.annotation.tailrec
+
 object MinAvgTwoSlice {
-  def solution(arr: Array[Int]): Int = {
-    var mindex: Int = 0
-    var minAvg: Double = 1000000
+  @tailrec
+  def findSlice(
+    arr: List[Int],
+    curindex: Int = 0,
+    mindex: Int = 0,
+    minAvg: Double = 1000000
+  ): Int =
+    arr match {
+      case _ :: t => {
+        val potential = Seq(arr.slice(0, 2), arr.slice(0, 3))
+          .filter(_.length > 1)
+          .map(x => (x.sum.toDouble / x.length))
+          .reduceOption(_ min _)
 
-    for (a <- List.range(0, arr.length - 1)) {
-      val potential = Seq(arr.slice(a, a + 2), arr.slice(a, a + 3))
-        .filter(_.length > 1)
-        .map(arr => (arr.sum.toDouble / arr.length))
-        .min
-
-      if (potential < minAvg) {
-        minAvg = potential
-        mindex = a
+        potential match {
+          case Some(p) if p < minAvg =>
+            findSlice(t, curindex + 1, curindex, p)
+          case _ =>
+            findSlice(t, curindex + 1, mindex, minAvg)
+        }
       }
+      case Nil => mindex
     }
 
-    mindex
-  }
+  def solution(arr: Array[Int]): Int =
+    findSlice(arr.toList)
 }
