7b32f1574a49806b00047f0d738e24b877b0f26e
Vit Brunner <vit.brunner@gmail.com>
2018-06-14 18:49:19 +0200

    Way faster

--- src/main/scala/_10/CountFactors.scala
+++ src/main/scala/_10/CountFactors.scala
@@ -5,7 +5,7 @@ import scala.annotation.tailrec
 object CountFactors {
   @tailrec
   def primeFactors(n: Int, cur: Int = 2, found: List[Int] = Nil): List[Int] =
-    if (cur > n) found
+    if (cur * cur > n) n :: found
     else if (n % cur == 0) primeFactors(n / cur, cur, cur :: found)
     else primeFactors(n, cur + 1, found)
 
