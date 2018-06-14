5b9a4d21eab57006da8891cd2c536b33e8c7ee01
Vit Brunner <vit.brunner@gmail.com>
2018-06-14 18:48:08 +0200

    Count factors

---
+++ src/main/scala/_10/CountFactors.scala
@@ -0,0 +1,16 @@
+package _10
+
+import scala.annotation.tailrec
+
+object CountFactors {
+  @tailrec
+  def primeFactors(n: Int, cur: Int = 2, found: List[Int] = Nil): List[Int] =
+    if (cur > n) found
+    else if (n % cur == 0) primeFactors(n / cur, cur, cur :: found)
+    else primeFactors(n, cur + 1, found)
+
+  def solution(a: Int): Int = {
+    val factors = primeFactors(a).groupBy(identity).values
+    factors.map(_.length).map(_ + 1).product
+  }
+}
---
+++ src/test/scala/_10/CountFactorsTest.scala
@@ -0,0 +1,39 @@
+package _10
+
+import org.scalatest.FlatSpec
+
+class CountFactorsTest extends FlatSpec {
+  it should "work for the sample case" in {
+    // 1 2 3 4 6 8 12 24
+    assert(CountFactors.solution(24) == 8)
+  }
+
+  it should "work for 49" in {
+    // 1 7 49
+    assert(CountFactors.solution(49) == 3)
+  }
+
+  it should "work for 13" in {
+    // 1 13
+    assert(CountFactors.solution(13) == 2)
+  }
+  it should "work for 75" in {
+    // 1 3 5 15 25 75
+    assert(CountFactors.solution(75) == 6)
+  }
+
+  it should "work for 45" in {
+    // 1 3 5 9 15 45
+    assert(CountFactors.solution(45) == 6)
+  }
+
+  it should "work for 32" in {
+    // 1 2 4 8 16 32
+    assert(CountFactors.solution(32) == 6)
+  }
+
+  it should "be fast for a large prime" in {
+    // large prime
+    assert(CountFactors.solution(780291637) == 2)
+  }
+}
