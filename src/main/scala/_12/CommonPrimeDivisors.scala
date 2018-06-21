daae2f8824d7f28403b0fbc5190338006a9ce443
Vit Brunner <vit.brunner@gmail.com>
2018-06-21 19:53:12 +0200

    Faster common prime factor checking

--- src/main/scala/_12/CommonPrimeDivisors.scala
+++ src/main/scala/_12/CommonPrimeDivisors.scala
@@ -1,16 +1,31 @@
 package _12
 
+import scala.annotation.tailrec
+
 import _10.CountFactors
 
 object CommonPrimeDivisors {
-  def primeFactors(n: Int): List[Int] =
-    CountFactors.primeFactors(n).distinct.sorted
+  @tailrec
+  def gcd(a: Int, b: Int): Int =
+    if (b == 0) a
+    else gcd(b, a % b)
 
-  def commonPrimeDivisors(a: Int, b: Int): Boolean =
-    primeFactors(a) == primeFactors(b)
+  def hasSamePrimeFactors(a: Int, b: Int): Boolean = {
+    val div = gcd(a, b)
+
+    if (div == 1) a == b
+    else {
+      val commonFactors = CountFactors.primeFactors(div)
+      val uniqueFactors =
+        CountFactors.primeFactors(a / div) ++
+        CountFactors.primeFactors(b / div)
+
+      uniqueFactors.forall(commonFactors.contains)
+    }
+  }
 
   def solution(a: Array[Int], b: Array[Int]): Int =
     (a zip b)
-      .map((commonPrimeDivisors _).tupled)
+      .map((hasSamePrimeFactors _).tupled)
       .count(identity)
 }
--- src/test/scala/_12/CommonPrimeDivisorsTest.scala
+++ src/test/scala/_12/CommonPrimeDivisorsTest.scala
@@ -1,5 +1,7 @@
 package _12
 
+import scala.util.Random
+
 import org.scalatest.FlatSpec
 
 class CommonPrimeDivisorsTest extends FlatSpec {
@@ -8,10 +10,30 @@ class CommonPrimeDivisorsTest extends FlatSpec {
   }
 
   it should "find common prime factors for 15 and 75" in {
-    assert(CommonPrimeDivisors.commonPrimeDivisors(15, 75) == true)
+    assert(CommonPrimeDivisors.hasSamePrimeFactors(15, 75) == true)
   }
 
   it should "not find common prime factors for 5 and 7" in {
-    assert(CommonPrimeDivisors.commonPrimeDivisors(5, 7) == false)
+    assert(CommonPrimeDivisors.hasSamePrimeFactors(5, 7) == false)
+  }
+
+  it should "not find common prime factors for 15 and 30" in {
+    // 3 * 5 vs 3 * 5 * 2
+    assert(CommonPrimeDivisors.hasSamePrimeFactors(15, 30) == false)
+  }
+
+  it should "work for 1 1" in {
+    assert(CommonPrimeDivisors.hasSamePrimeFactors(1, 1) == true)
+  }
+
+  it should "work for 5 5" in {
+    assert(CommonPrimeDivisors.hasSamePrimeFactors(5, 5) == true)
+  }
+
+  it should "be fast" in {
+    CommonPrimeDivisors.solution(
+      Array.fill(10000)(Random.nextInt(1000)),
+      Array.fill(10000)(Random.nextInt(1000))
+    )
   }
 }
